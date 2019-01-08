82ecc5e358241f614ffd92395e8ada7c5746d41b
Devin Sweeting and ngan nguyen <devin+ngan@>
2019-01-08 12:00:06 -0800

    add karma test for two players and for win conditions

--- spec/pigdice-spec.js
+++ spec/pigdice-spec.js
@@ -1,9 +1,42 @@
 import { Players } from './../src/pigdice.js';
 
 describe('Players', function() {
+  var playerOne;
+
+  beforeEach(function() {
+    playerOne = new Players("Devin");
+  });
 
   it('It should grab user input as player name', function () {
-    var playerOne = new Players("Devin");
-    expect(playerOne.checkType()).toEqual("Devin");
+    expect(playerOne.user).toEqual("Devin");
+  });
+
+  it('It should grab rollDi function', function () {
+    expect(playerOne.rollDi()).toBeLessThan(7);
+  });
+
+  it('It should hold the players score', function () {
+    var turn = playerOne.rollDi();
+    playerOne.hold();
+    expect(playerOne.total).toEqual(turn);
+  });
+
+  it('Should test if a player has won', function(){
+    playerOne.total = 23;
+    playerOne.hold();
+    spyOn(window, 'alert');
+    expect(window.alert).toHaveBeenCalledWith('You Win!')
+  });
+
+  it('create two Players', function(){
+    var playerTwo = new Players("Tavish");
+    expect(playerOne.user).toEqual("Devin");
+    expect(playerTwo.user).toEqual("Tavish");
+  });
+
+  it('will start game', function(){
+    var playerTwo = new Players("Tavish");
+    expect(playerOne.user).toEqual("Devin");
+    expect(playerTwo.user).toEqual("Tavish");
   });
 });
